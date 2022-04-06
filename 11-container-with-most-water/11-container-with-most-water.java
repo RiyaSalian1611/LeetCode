e19b01d8d23b9f3ad543256fd2d040cdba93df8c
Riya Salian <51844886+RiyaSalian1611@users.noreply.github.com>
2022-04-06 09:23:36 +0530

    Time: 5 ms (43.91%), Space: 73.4 MB (71.08%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -4,9 +4,8 @@ class Solution {
         int ptr2 = height.length-1;
         int maxArea = 0;
         
-        while(ptr1 < ptr2){
-            int lHeight = Math.min(height[ptr1], height[ptr2]);            
-            maxArea = Math.max(maxArea, lHeight*(ptr2-ptr1));
+        while(ptr1 < ptr2){            
+            maxArea = Math.max(maxArea, Math.min(height[ptr1], height[ptr2])*(ptr2-ptr1));
             if(height[ptr2] < height[ptr1])
                 ptr2--;
             else
