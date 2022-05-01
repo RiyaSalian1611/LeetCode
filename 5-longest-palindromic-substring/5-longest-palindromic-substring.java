79fd038271f15f5e84c5d3f52b2d942fb427a880
Riya Salian <51844886+RiyaSalian1611@users.noreply.github.com>
2022-05-01 22:40:49 +0530

    Time: 669 ms (11.74%), Space: 275.1 MB (5.00%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,24 +1,21 @@
 class Solution {
+    String longestPalindrome = "";
     public String longestPalindrome(String s) {
-        String longestPalindrome = "";
+       
         int mid = s.length()/2;
         int start = 0 ; int end = s.length()-1;
         for(int i=0; i< s.length(); i++){
-            //odd length
-            start =i;  end = i;
-            while(start>=0 && end<=s.length()-1 && Character.compare(s.charAt(start),s.charAt(end))==0){
-                longestPalindrome = s.substring(start, end+1).length() > longestPalindrome.length() ? s.substring(start, end+1) : longestPalindrome;
-                start--;
-                end++;
-            }
-            //even length
-            start =i;  end = i+1;
-            while(start>=0 && end<=s.length()-1 && Character.compare(s.charAt(start),s.charAt(end))==0){
-                longestPalindrome = s.substring(start, end+1).length() > longestPalindrome.length() ? s.substring(start, end+1) : longestPalindrome;
-                start--;
-                end++;
-            }
+            isPalindrome(i, i, s);
+            isPalindrome(i, i+1, s);
         }
         return longestPalindrome;
+    }
+    
+    private void isPalindrome(int start, int end, String s){
+        while(start>=0 && end<=s.length()-1 && Character.compare(s.charAt(start),s.charAt(end))==0){
+                longestPalindrome = s.substring(start, end+1).length() > longestPalindrome.length() ? s.substring(start, end+1) : longestPalindrome;
+                start--;
+                end++;
+            }
     }
 }
