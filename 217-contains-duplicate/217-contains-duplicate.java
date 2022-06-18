f2de0bfb3757816775b15d581fba14080cbad533
Riya Salian <51844886+RiyaSalian1611@users.noreply.github.com>
2022-06-18 18:37:33 +0530

    Time: 8 ms (84.28%), Space: 54.4 MB (93.57%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -3,9 +3,11 @@ import java.util.*;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i]==nums[i+1])
+        Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+            if(!ht.containsKey(nums[i]))
+                ht.put(nums[i], nums[i]);
+            else
                 return true;
         }
         return false;
