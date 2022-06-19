e52db8e2e74f5e8731a6e81bdc837b595ede9eca
Riya Salian <51844886+RiyaSalian1611@users.noreply.github.com>
2022-06-19 12:38:43 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (67.89%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,41 +9,15 @@
  * }
  */
 class Solution {
+    public ListNode reverseList(ListNode head) {
     
-    ListNode dummyHead = new ListNode(0);
-        ListNode prev_list = dummyHead.next;
-        int counter = 0;
-    
-    public ListNode reverseList(ListNode l1) {
-        /** ITERATIVE AND RECURSIVE */
-        
-    /*********ITERATIVE***************
-    /** l1 is head ****
-    
-        Stack<ListNode> s = new Stack<ListNode>();
-        ListNode ln = l1;
-        ListNode dummyHead = new ListNode(0);
-        ListNode result = dummyHead;
-        
-        while(ln!=null){
-            s.push(new ListNode(ln.val));
-            ln = ln.next;
+        ListNode prev = null;
+        while(head!=null){
+            ListNode nextHead = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextHead;
         }
-        while(!s.isEmpty()){
-            result.next = s.pop();
-            result = result.next;
-        }
-        return dummyHead.next;
-    */
-        
-    /********RECURSIVE*************/
-            
-            if(l1==null) return prev_list;           
-            ListNode newNode = new ListNode(l1.val);
-            newNode.next = prev_list;
-            prev_list = newNode;
-            l1 = l1.next;
-            reverseList(l1);   
-        return prev_list;
+            return prev;
     }
 }
