4c3a710d7e3ad87c4eb6fe2265cfbc887c622810
Riya Salian <51844886+RiyaSalian1611@users.noreply.github.com>
2022-07-03 20:44:56 +0530

    Time: 81 ms (19.19%), Space: 55.8 MB (6.29%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.java
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.java
@@ -1,42 +1,17 @@
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        int n = nums.length;
-        if (n<=1) return false;
-        if (t<0) return false;
-        
-        // take care when t == 0... Bucket Sort
-        long width = (long)t+1;
-        
-        Map<Long,Long> map = new HashMap<>();
-        for (int i = 0; i < n; i ++)
-        {
-            long bucketId = getBucketId((long)nums[i],width);
-            if (map.containsKey(bucketId))
-            {
+       TreeMap<Long,Integer> treemap=new TreeMap<>();        
+        for(int i=0;i<nums.length;i++){
+            if(treemap.size() > k) 
+                treemap.remove((long)nums[i - k - 1]);
+            Long ceil = treemap.ceilingKey((long)nums[i]);
+            Long floor = treemap.floorKey((long)nums[i]);
+            if(ceil != null && ceil <= (long)nums[i] + t)
                 return true;
-            }
-            if (map.containsKey(bucketId-1l) && Math.abs(map.get(bucketId-1)-nums[i])<width)
-            {
+            if(floor != null && floor >= (long)nums[i] - t)
                 return true;
-            }
-            if (map.containsKey(bucketId+1l) && Math.abs(map.get(bucketId+1)-nums[i])<width)
-            {
-                return true;
-            }
-            map.put(bucketId,(long)nums[i]);
-            if (i-k>=0)
-            {
-                long oldBucketId = getBucketId((long)nums[i-k],width);
-                map.remove(oldBucketId);
-            }
+            treemap.put((long)nums[i],i);
         }
-        
         return false;
-    }
-    
-    private long getBucketId(long val, long width)
-    {
-        return val < 0 ? (val+1l)/width-1l : val/width;
-    }
-    
+    } 
 }
