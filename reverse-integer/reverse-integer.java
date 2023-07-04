dbcb451a6115ea851c7e67c8467cc1cc181cc53c
Riya Salian <51844886+RiyaSalian1611@users.noreply.github.com>
2023-07-03 17:09:46 -0700

    Time: 1 ms, Memory: 40.1 MB - LeetHub

--- reverse-integer/reverse-integer.java
+++ reverse-integer/reverse-integer.java
@@ -1,21 +1,10 @@
 class Solution {
     public int reverse(int x) {
-        String s = "";
-        int sign = 1;
-        int start = 0;
-        String s1 = String.valueOf(x);
-        
-        if (s1.charAt(0)=='-'){
-            start=1; sign= -1;}
-        
-        for (int i=start; i<s1.length(); i++){
-            char ch = s1.charAt(i);
-            s = ch+ s;
-        }
-        
-        if (Long.parseLong(s)> Integer.MAX_VALUE)
-            return 0;
-        else
-            return Integer.parseInt(s)*sign;
+    long ans = 0;
+    while (x!=0) {
+      ans = ans * 10 + x % 10;
+      x /= 10;
+    }
+    return (ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE) ? 0 : (int)ans;
     }
 }
